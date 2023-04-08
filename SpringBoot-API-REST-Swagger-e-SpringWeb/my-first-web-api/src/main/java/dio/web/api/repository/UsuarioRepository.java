33c769c30c737c423d23d7bcc10554582ffc1f28
Call333 <cam4@aluno.ifal.edu.br>
2023-04-08 16:36:32 -0300

    Add o curso criando uma api rest com swagger

---
+++ SpringBoot-API-REST-Swagger-e-SpringWeb/my-first-web-api/src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -0,0 +1,46 @@
+package dio.web.api.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import dio.web.api.model.Usuario;
+
+@Repository
+public class UsuarioRepository {
+
+    public void save(Usuario usuario){
+        if(usuario.getId()==null)
+          System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        else
+         System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        
+        System.out.println(usuario);
+    }
+
+    public void deleteById(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+
+    public List<Usuario> findAll(){
+        System.out.println("LIST - Listando os usários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1,"gleyson","password"));
+        usuarios.add(new Usuario(2,"frank","masterpass"));
+        usuarios.add(new Usuario(3, "calebe", "abobora"));
+        usuarios.add(new Usuario(4, "bigger", "abobora"));
+        return usuarios;
+    }
+
+    public Usuario findById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario(5,"gleyson","password");
+    }
+
+    public Usuario findByUsername(String username){
+        System.out.println(String.format("FIND/username - Recebendo o usernamae: %s para localizar um usuário", username));
+        return new Usuario(6, "calebe","password");
+    }
+}
